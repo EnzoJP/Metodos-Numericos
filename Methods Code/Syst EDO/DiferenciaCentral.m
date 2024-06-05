d227bdc09452dc72d877a588e0fc984cc33e759b
Enzo Palau <enzoopalau@gmail.com>
2024-06-05 12:32:36 -0300

    si

--- Methods Code/Syst EDO/DiferenciaCentral.m
+++ Methods Code/Syst EDO/DiferenciaCentral.m
@@ -50,40 +50,62 @@ function fy=fun_ind(x,G,Dt)
   fy=Dt^2*G*r;
 end
 
-
-##function DFCSistema
-##  M=[1,0,0;0,-1,0;0,0,2];
-##  C=[4,0,0;0,-1,0;0,0,3];
-##  K=[0,4,1;4,2,0;1,0,1];
-##  dt=0.01;
-##  t1=0;
-##  tfinal=3;
 ##
-##  x=t1:dt:tfinal;
+##function dif_central
+##  % Datos
+##  Dt=1/10; % incremento de tiempo
+##  NDt=1500; % cantidad de Dt a realizar
+##  M=[ 1 0 0 0; 0 1 0 0; 0 0 1 0; 0 0 0 1] ; % Matriz M
+##  C=[ 0 0 0 0; 0 0 0 0; 0 0 0 0 ; 0 0 0 0]; % Matriz C
+##  K=[ 2 -1 0 0; -1 2 -1 0 ;0 -1 2 -1; 0 0 -1 2]; % Matriz K
+##  b=[10; 5; 5; 5];
 ##
-##  GenCero=[1;2;1];
-##  GPrimaEnT0=[1;4;0];
+##  % Valores Iniciales o actuales
+##  tac=0;
+##  f=[0; 0; 0; 0]; % correspondiente a y1
+##  df=[0; 0; 0; 0]; % correspondiente a y2
+##  R(1,1)=b(1,1);
+##  R(2,1)=b(2,1);
+##  R(3,1)=b(3,1);
+##  R(4,1)=b(4,1);
+##  % Inicialización
+##  G=inv(M+(Dt/2)*C); % cálculo del término G
+##  D=G*(2*M-Dt^2*K); % cálculo del término D
+##  H=G*((Dt/2)*C-M); % cálculo del término H
+##  d2=inv(M)*(R-K*f-C*df);% cálculo de la derivada segunda
+##  yan=f-Dt*df+((Dt^2)/2)*d2; % cálculo de solución anterior
+##  t(1)=tac; % Almacenamiento para luego graficar
+##  y(:,1)=f;
 ##
-##  R(1,1)=5*(e^0)+8*(e^(2*0))+cos(0);
-##  R(2,1)=(-8*(e^2*0))+4*(e^0);
-##  R(3.1)=-cos(0)-3*sin(0)+(e^0);
+##  % Diferencia Central
+##  for j=2:NDt
+##      bac=fun_ind(tac, G, Dt); % cálculo de b que depende del tiempo
+##      ynu=bac + D*f + H*yan; % Calculo con Dif Central
+##      tnu=tac+Dt; % actualización del tiempo
+##      t(j)=tnu; % Almacenamiento para luego graficar
+##      y(:,j)=ynu; % almacenamiento de la solución para luego graficar
+##      yan=f; % actualización de estado anterior
+##      f=ynu; % actualización de estado actual
+##      tac=tnu;
+##  end
 ##
-##  G=inv(M+(dt/2)*C);
-##  D=G*(2*M - (dt^2)*K);
-##  H=G*((dt/2)*C-M);
-##  b=((dt)^2)*G*R;
+##  % Gráfico
+##  figure(1)
+##  plot( t, y(1,:))#,'g', t, y(2,:),'b', t, y(3,:),'r');
+##  grid on
 ##
-##  GPrima2= (R-(C.*GPrimaEnT0) - (K.*GenCero))*inv(M);
+##  disp(y(:,51))
+##end
 ##
-##  PAnterior=GenCero-dt*GPrimaEnT0+(((dt)^2)/2)*GPrima2;
-##
-##  for i=1:length(x)
-##    R(1,i)=5*(e^x(i))+8*(e^2*x(i))+cos(x(i));
-##    R(2,i)=(-8*(e^2*x(i)))+4*(e^x(i));
-##    R(3,i)=-cos(x(i))-3*sin(x(i))+(e^x(i));
-##
-##    PNuevo=b(x(i))+D*GenCero+H*PAnterior
-##  endfor
-##
-##
-##endfunction
+##% Subfunción para determinar el término g
+##function fy=fun_ind(t, G, dt)
+##  if (t<50.1)
+##    r(1,1)=10*((-1/50.1)*t + 1);
+##    r(2,1)=5*((-1/50.1)*t + 1);
+##    r(3,1)=5*((-1/50.1)*t + 1);
+##    r(4,1)=5*((-1/50.1)*t + 1);
+##    fy=dt^2*G*r;
+##  else
+##    fy=0;
+##  end
+##end
